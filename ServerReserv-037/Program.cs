6364137da5a45a8e7d954bba603244cc1f150ce0
annis <nissinkentang@gmail.com>
2021-12-28 03:25:20 +0700

    Add project files.

---
+++ ServerReserv-037/Program.cs
@@ -0,0 +1,32 @@
+﻿using ServiceReservasi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerReserv_037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Service1));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch(Exception e)
+            {
+                hostObj = null;
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
